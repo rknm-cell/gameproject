5ef3e27f12b8e31c980a355f31d89be29b5db310
rknm-cell <racknahm@gmail.com>
2025-06-02 17:49:15 -0400

    added front end styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,38 @@
-import React from "react"
-import { useState } from "react"
+
+import { useState } from "react";
+import { NewGame } from "./game/gameEngine";
+import { playerMove } from "./game/gameEngine";
 
 function App() {
-  const [square, setSquare] = useState([]);
-  const [player, setPlayer] = useState('');
-  return (
-    <div>
-    TicTacToe
-    <div>
+  const [game, setGame] = useState(NewGame);
 
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <h1 className="text-3xl font-bold mb-4">TicTacToe</h1>
+      <div>
+        <h2>{game.player}</h2>
+      </div>
+
+      <div className="grid grid-flow-col grid-rows-3 gap-3">
+        <div>
+          <button onClick={() => playerMove(game, 0)}>{game.board[0]}</button>
+          <button onClick={() => playerMove(1)}>{game.board[1]}</button>
+          <button onClick={() => playerMove(2)}>{game.board[2]}</button>
+        </div>
+        <div>
+          <button onClick={() => playerMove(0)}>{game.board[0]}</button>
+          <button onClick={() => playerMove(1)}>{game.board[1]}</button>
+          <button onClick={() => playerMove(2)}>{game.board[2]}</button>
+        </div>
+        <div>
+          <button onClick={() => playerMove(0)}>{game.board[0]}</button>
+          <button onClick={() => playerMove(1)}>{game.board[1]}</button>
+          <button onClick={() => playerMove(2)}>{game.board[2]}</button>
+        </div>
+      </div>
+      <button>New Game</button>
     </div>
-     
-    </div>
-  )
+  );
 }
 
-export default App
+export default App;
