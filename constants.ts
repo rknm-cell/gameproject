dc126685ea9e0a30d40c07276135b068e2d370b2
rknm-cell <racknahm@gmail.com>
2025-06-07 15:42:19 -0400

    changed client url in constants

--- constants.ts
+++ constants.ts
@@ -1,3 +1,3 @@
 export const PORT=3000
 export const SERVER_URL = `https://gameproject-tnpvp.kinsta.app`
-export const CLIENT_URL = `http://localhost:5173`
+export const CLIENT_URL = `https://tictactoe-blush-chi.vercel.app/`
