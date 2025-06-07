56e7a6788883e0a463f9537e8e6c9b9f9fc4bf25
rknm-cell <racknahm@gmail.com>
2025-06-07 15:45:02 -0400

    removed slash

--- constants.ts
+++ constants.ts
@@ -1,3 +1,3 @@
 export const PORT=3000
 export const SERVER_URL = `https://gameproject-tnpvp.kinsta.app`
-export const CLIENT_URL = `https://tictactoe-blush-chi.vercel.app/`
+export const CLIENT_URL = `https://tictactoe-blush-chi.vercel.app`
